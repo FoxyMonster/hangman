9a0ed435433a1a674c011122d4181fbbf844fe71
FoxyMonster <44702461+FoxyMonster@users.noreply.github.com>
2018-11-13 15:17:34 +0100

    program files

---
+++ szubienica.js
@@ -0,0 +1,119 @@
+
+var haslo = "";
+var haslo1 = "";
+var dlugosc;
+
+//zapisanie hasła wpisanego przez gracza//
+function zapisz(){
+haslo = document.getElementById("pole").value;
+haslo = haslo.toLocaleUpperCase();
+dlugosc = haslo.length;
+for(i=0;i<dlugosc;i++){
+    if(haslo.charAt(i)==" ") haslo1=haslo1+" ";
+    else haslo1=haslo1+"-";
+}
+document.getElementById("tekst").innerHTML="";
+document.getElementById("gibbet").innerHTML='<img id="draw" src="szubienica/s0.jpg"/>';
+start();
+}
+
+var ile_skuch=0;
+
+var yes=new Audio("szubienica/yes.wav");
+var no=new Audio("szubienica/no.wav");
+
+function wypisz_haslo(){
+    document.getElementById("board").innerHTML=haslo1;
+}
+
+//window.onload=start;//
+var litery = new Array(35);
+litery[0]="A";
+litery[1]="Ą";
+litery[2]="B";
+litery[3]="C";
+litery[4]="Ć";
+litery[5]="D";
+litery[6]="E";
+litery[7]="Ę";
+litery[8]="F";
+litery[9]="G";
+litery[10]="H";
+litery[11]="I";
+litery[12]="J";
+litery[13]="K";
+litery[14]="L";
+litery[15]="Ł";
+litery[16]="M";
+litery[17]="N";
+litery[18]="Ń";
+litery[19]="O";
+litery[20]="Ó";
+litery[21]="P";
+litery[22]="Q";
+litery[23]="R";
+litery[24]="S";
+litery[25]="Ś";
+litery[26]="T";
+litery[27]="U";
+litery[28]="V";
+litery[29]="W";
+litery[30]="Y";
+litery[31]="X";
+litery[32]="Z";
+litery[33]="Ź";
+litery[34]="Ż";
+
+function start(){
+    
+    var tresc_diva="";
+    for(i=0;i<=34;i++){
+        var element = "lit"+i;
+        tresc_diva=tresc_diva+'<div class="litera" onclick="sprawdz('+i+')"id="'+element+'">'+litery[i]+'</div>';
+    }
+
+    document.getElementById("alphabet").innerHTML=tresc_diva;
+    wypisz_haslo();
+}
+
+String.prototype.ustawZnak=function(miejsce, znak){
+    if(miejsce>this.length-1) return this.toString();
+    else return this.substr(0, miejsce)+znak+this.substr(miejsce+1);
+}
+
+function sprawdz(nr){
+    var trafiona=false;
+    for(i=0; i<dlugosc; i++){
+        if(haslo.charAt(i)==litery[nr]){
+            haslo1=haslo1.ustawZnak(i,litery[nr]);
+            trafiona=true;
+        }
+    }
+    if(trafiona==true){
+        yes.play();
+        var element = "lit"+nr;
+        document.getElementById(element).style.background="#003300";
+        document.getElementById(element).style.color="#00C000";
+        document.getElementById(element).style.border="2px solid #00C000";
+        document.getElementById(element).style.cursor="default";
+        document.getElementById(element).style.cursor="not-allowed";
+        wypisz_haslo();
+    }
+    else{
+        no.play();
+        var element = "lit"+nr;
+        document.getElementById(element).style.background="#330000";
+        document.getElementById(element).style.color="#C00000";
+        document.getElementById(element).style.border="2px solid #C00000";
+        document.getElementById(element).style.cursor="not-allowed";
+        document.getElementById(element).setAttribute("onclick",";");
+        ile_skuch++;
+        var obraz="szubienica/s"+ile_skuch+".jpg";
+        document.getElementById("gibbet").innerHTML='<img id="draw" src="'+obraz+'" alt=""/>';
+    }
+    if (haslo==haslo1)
+    document.getElementById("alphabet").innerHTML='Gratulacje!<br/><br/><span class="reset" onclick="location.reload()">JESZCZE RAZ?</span>';
+    
+    if(ile_skuch>=9)
+    document.getElementById("alphabet").innerHTML='Przegrana!<br/><br/><span class="reset" onclick="location.reload()">SPRÓBUJ JESZCZE RAZ</span>';
+}
